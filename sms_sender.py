deb399eb75ccc6489b2ebe437fd1f67e43e8434e
MarioMercurio <mercuriom@xavier.edu>
2026-03-16 14:14:40 -0400

    Update sms_sender.py

--- sms_sender.py
+++ sms_sender.py
@@ -1,17 +1,20 @@
-import os
 from twilio.rest import Client
+import streamlit as st
 
-def send_sms(message):
 
-    account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-    auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-    from_phone = os.getenv("TWILIO_PHONE")
-    to_phone = os.getenv("ALERT_PHONE")
+def send_sms(message: str):
+
+    account_sid = st.secrets["TWILIO_ACCOUNT_SID"]
+    auth_token = st.secrets["TWILIO_AUTH_TOKEN"]
+    from_number = st.secrets["TWILIO_FROM_NUMBER"]
+    to_number = st.secrets["TWILIO_TO_NUMBER"]
 
     client = Client(account_sid, auth_token)
 
-    client.messages.create(
+    message = client.messages.create(
         body=message,
-        from_=from_phone,
-        to=to_phone
+        from_=from_number,
+        to=to_number
     )
+
+    return message.sid
