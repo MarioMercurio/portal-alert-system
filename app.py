845f4a038b33237947a103cb01d4b81ddc60f9c7
MarioMercurio <mercuriom@xavier.edu>
2026-03-16 10:29:17 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Portal Alert System", page_icon="🚨")
+
+st.title("Portal Alert System")
+st.write("Build in progress.")
+st.write("This app is separate from your Player Portal site.")
