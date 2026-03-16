0ca9e6986ddaecb4fb16635fa75b878bc9e65c96
MarioMercurio <mercuriom@xavier.edu>
2026-03-16 10:46:23 -0400

    Update tweet_parser.py

--- tweet_parser.py
+++ tweet_parser.py
@@ -2,10 +2,10 @@ import re
 
 def extract_player_name(tweet_text):
     """
-    Detect player names including initials like A.J. Storr
+    Detect player names including initials like A.J. Storr or DJ Wagner
     """
 
-    pattern = r'\b(?:[A-Z]\.)?[A-Z][a-z]+(?:\s+[A-Z][a-zA-Z\'\-]+)\b'
+    pattern = r'\b(?:[A-Z]\.[A-Z]\.|[A-Z][a-z]+)\s+[A-Z][a-zA-Z\'\-]+\b'
 
     matches = re.findall(pattern, tweet_text)
 
